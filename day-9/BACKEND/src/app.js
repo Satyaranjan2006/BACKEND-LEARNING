674eb525a620ddfc11e8510aa11af3ab1b030b1b
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-06-08 10:28:10 +0530

    Add initial setup for React frontend with Vite
    - Created main entry point (main.jsx) for the React application.
    - Added global styles in index.css for consistent layout and design.
    - Included React and Vite configuration files (vite.config.js).
    - Added SVG assets for React and Vite logos.
    -Set up call for fetch notes and displaythem in app

---
+++ day-9/BACKEND/src/app.js
@@ -0,0 +1,71 @@
+const express=require('express')
+const noteModel=require('./models/note.model')
+const cors=require('cors')
+
+
+
+const app=express()
+app.use(express.json())
+app.use(cors())
+
+/*
+
+post /api/notes
+Create  new note and save data in mongodb
+*/
+app.post('/notes',async(req,res)=>{
+    //destructur
+    const{title,description}=req.body
+    
+    //storing in database
+    const note=await noteModel.create({
+        title,description
+    })
+    res.status(201).json({
+        message:'note created successfully',
+        note
+    })
+})
+
+app.get('/notes',async(req,res)=>{
+    const notes=await noteModel.find()
+
+    res.status(200).json({
+        message:'note fetched successfully',
+        notes
+    })
+
+})
+
+/**
+ * delet the note via id 
+ */
+app.delete('/notes/:id',async(req,res)=>{
+    const id=req.params.id
+
+    const notes=await noteModel.findByIdAndDelete(id)
+
+    res.status(200).json({
+        message:'note deleted successfully',
+        notes
+    })
+})
+
+/*
+update the note via id
+
+*/
+app.patch('/notes/:id',async(req,res)=>{
+    const id=req.params.id
+    const{title,description}=req.body
+
+    const notes=await noteModel.findByIdAndUpdate(id,{title,description})
+
+    res.status(200).json({
+        message:'note updated successfully',
+        notes
+    })
+})
+
+
+module.exports=app
