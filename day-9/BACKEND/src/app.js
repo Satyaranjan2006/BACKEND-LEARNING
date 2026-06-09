878f6c79e75a83e1b9c9b35b63d3c39b2e6c05c1
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-06-09 20:08:17 +0530

    Add initial HTML and CSS files for frontend layout
    - Created index.html with basic structure and linked assets
    - Added index-CFi-oNV_.css for styling the application

--- day-9/BACKEND/src/app.js
+++ day-9/BACKEND/src/app.js
@@ -1,12 +1,15 @@
 const express=require('express')
 const noteModel=require('./models/note.model')
 const cors=require('cors')
+const path=require('path')
 
 
 
 const app=express()
 app.use(express.json())
 app.use(cors())
+
+app.use(express.static('./public'))
 
 /*
 
@@ -67,5 +70,10 @@ app.patch('/notes/:id',async(req,res)=>{
     })
 })
 
+//handling wildcart concept
+
+app.use('*name',(req,res)=>{
+    res.sendFile(path.join(__dirname,'..', '/public/index.html'))
+})
 
 module.exports=app
--- day-9/FRONTEND/frontend/src/App.jsx
+++ day-9/FRONTEND/frontend/src/App.jsx
@@ -1,52 +1,80 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 
 function App() {
 
- 
-  const [notes, setNotes] = useState([
-    {
-    title:'test title 1',
-    description:'test description 1'
-   },
-   {
-    title:'test title 2',
-    description:'test description 2'
-   },
-   {
-    title:'test title 3',
-    description:'test description 3' 
-   },
-   {
-    title:'test title 4',
-    description:'test description 4'
-   },
-])
 
-axios.get('http://localhost:3000/notes')
-.then((res)=>{
-  setNotes(res.data.notes)
-})
+  const [notes, setNotes] = useState([])
+  console.log('hello integration');
 
- 
+  function fetchNotes() {
+    axios.get('http://localhost:3000/notes')
+      .then((res) => {
+        setNotes(res.data.notes)
+      })
 
-  
+  }
+
+
+  useEffect(() => {
+
+    fetchNotes()
+
+  }, [])
+
+  function handelSubmit(e) {
+    e.preventDefault()
+    const { title, description } = e.target.elements
+    console.log(title.value, description.value)
+
+    axios.post('http://localhost:3000/notes', {
+      title: title.value,
+      description: description.value
+    })
+      .then(res => {
+        console.log(res.data);
+        fetchNotes()
+      })
+
+  }
+  function handelDeleteNote(noteId) {
+
+    axios.delete('http://localhost:3000/notes/'+noteId)
+      .then(res => {
+        console.log(res.data);
+        fetchNotes()
+
+      })
+
+  }
+
+
+
+
+
   return (
     <>
-    <div className="notes">
-      {
-        notes.map(note=>{
-         return <div className="note">
-             <h1>{note.title}</h1>
-             <p>{note.description}</p>
-      </div>
+      <form className='note-created-form' onSubmit={handelSubmit}>
+        <input name='title' type="text" placeholder='Enter title' />
+        <input name='description' type="text" placeholder='Enter Description' />
+        <button>Create Notes</button>
+      </form>
+      <div className="notes">
+        {
+          notes.map(note => {
+            return <div className="note">
+              <h1>{note.title}</h1>
+              <p>{note.description}</p>
+              {/* helps to deleete */}
+              <button onClick={() => { handelDeleteNote(note._id) }}>delete</button>
+            </div>
 
-        })
-      }
-      
-      
-    </div>
+          })
+        }
+
+
+      </div>
     </>
   )
 }
