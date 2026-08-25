f313aa7f1d572361129a16463db82eec0ca2d55c
satyaranjan2006 <satyaranjan9622@gmail.com>
2026-08-25 16:38:34 +0530

    prepairing for integration

---
+++ day-14/Frontend/frontend/src/routes.js
@@ -0,0 +1,12 @@
+import {BrowserRouter} from 'react-router'
+
+ export const routes=BrowserRouter([
+    {
+        path:'/login',
+        element:""
+    },
+    {
+        path:'/register',
+        element:""
+    }
+])
